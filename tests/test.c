94722bab18de5575f86f9a9eaf5794779c587e87
Alfret <Afisse13@gmail.com>
2019-03-09 11:26:37 +0100

    Fixed invalid string encoding in test file
    * Strings in the test file were not encoded in valid UTF-8 which meant
    that the tests failed even though they should pass

--- tests/test.c
+++ tests/test.c
@@ -76,21 +76,21 @@ uint32_t numbers0through79[] = {
 void test_alfUTF8Insert(AlfTestState* state)
 {
 	// Setup input strings
-	const char* input0 = "mŒndag";
+	const char* input0 = u8"måndag";
 
 	// Insert
-	char* output0 = alfUTF8Insert(input0, 3, 0, "ads");
-	ALF_CHECK_STREQ_R(state, output0, "mŒnadsdag",
+	char* output0 = alfUTF8Insert(input0, 3, 0, u8"ads");
+	ALF_CHECK_STREQ_R(state, output0, u8"månadsdag",
 		"Add letters in word, no delete");
 
 	// Delete
-	char* output1 = alfUTF8Insert(input0, 3, 3, "");
-	ALF_CHECK_STREQ_R(state, output1, "mŒn",
+	char* output1 = alfUTF8Insert(input0, 3, 3, u8"");
+	ALF_CHECK_STREQ_R(state, output1, u8"mån",
 		"Only delete letters, no adding");
 
 	// Replace
-	char* output2 = alfUTF8Insert(input0, 0, 3, "annan");
-	ALF_CHECK_STREQ_R(state, output2, "annandag",
+	char* output2 = alfUTF8Insert(input0, 0, 3, u8"annan");
+	ALF_CHECK_STREQ_R(state, output2, u8"annandag",
 		"Replace letters, delete some and add some");
 }
 
@@ -99,37 +99,37 @@ void test_alfUTF8Insert(AlfTestState* state)
 void test_alfUTF8Substring(AlfTestState* state)
 {
 	// Setup input strings
-	const char* input0 = "måndag";
+	const char* input0 = u8"måndag";
 
 	// Check strings that starts at beginning
 	char* output0 = alfUTF8Substring(input0, 0, 0); // Empty word
-	ALF_CHECK_STREQ_R(state, output0, "",
+	ALF_CHECK_STREQ_R(state, output0, u8"",
 		"Empty substring at beginning of string");
 	char* output1 = alfUTF8Substring(input0, 0, 1); // First letter
-	ALF_CHECK_STREQ_R(state, output1, "m",
+	ALF_CHECK_STREQ_R(state, output1, u8"m",
 		"First letter at beginning of string");
 	char* output2 = alfUTF8Substring(input0, 0, 2); // First 2 letters
-	ALF_CHECK_STREQ_R(state, output2, "mŒ",
+	ALF_CHECK_STREQ_R(state, output2, u8"må",
 		"First two letters at beginning of string");
 	char* output3 = alfUTF8Substring(input0, 0, 5); // All except 1 letter
-	ALF_CHECK_STREQ_R(state, output3, "mŒnda",
+	ALF_CHECK_STREQ_R(state, output3, u8"månda",
 		"Entire string expect last letter");
 	char* output4 = alfUTF8Substring(input0, 0, 6); // All
-	ALF_CHECK_STREQ_R(state, output4, "mŒndag",
+	ALF_CHECK_STREQ_R(state, output4, u8"måndag",
 		"Entire string");
 
 	// Check strings that ends at the end
 	char* output5 = alfUTF8Substring(input0, 5, 0); // Empty word
-	ALF_CHECK_STREQ_R(state, output5, "",
+	ALF_CHECK_STREQ_R(state, output5, u8"",
 		"Empty substring at beginning of string");
 	char* output6 = alfUTF8Substring(input0, 5, 1); // First letter
-	ALF_CHECK_STREQ_R(state, output6, "g",
+	ALF_CHECK_STREQ_R(state, output6, u8"g",
 		"First letter at beginning of string");
 	char* output7 = alfUTF8Substring(input0, 4, 2); // First 2 letters
-	ALF_CHECK_STREQ_R(state, output7, "ag",
+	ALF_CHECK_STREQ_R(state, output7, u8"ag",
 		"First two letters at beginning of string");
 	char* output8 = alfUTF8Substring(input0, 1, 5); // All except 1 letter
-	ALF_CHECK_STREQ_R(state, output8, "Œndag",
+	ALF_CHECK_STREQ_R(state, output8, u8"åndag",
 		"Entire string expect last letter");
 
 	// Invalid index
